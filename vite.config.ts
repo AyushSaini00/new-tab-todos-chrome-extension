ff8dfa1c319989124854962ec670e35ad42cfff5
Ayush Saini <ayushsainiiitb00@gmail.com>
2025-09-19 10:42:01 +0530

    remove dir from build dist on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
+import * as path from "path";
+import fs from "fs";
+
+const removeFolderFromDist = (folderName: string) => {
+  return {
+    name: "remove-folder-from-dist",
+    writeBundle(outputOptions: any) {
+      const outDir = outputOptions.dir;
+      const folderToRemove = path.resolve(outDir, folderName);
+
+      if (fs.existsSync(folderToRemove)) {
+        fs.rm(folderToRemove, { recursive: true, force: true }, (err) => {
+          if (err) {
+            console.error(`Failed to delete ${folderName}: `, err);
+          } else {
+            console.log(`removed ${folderName} from dist`);
+          }
+        });
+      }
+    },
+  };
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
+  plugins: [tailwindcss(), svelte(), removeFolderFromDist("screenshots")],
 });
