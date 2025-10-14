c0200228e2712d1c5e830c4f83d98c2425043dd5
Ayush Saini <ayushsainiiitb00@gmail.com>
2025-10-14 11:59:30 +0530

    fixing heap issue

--- src/components/TodoList.svelte
+++ src/components/TodoList.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
   import type { Todo } from "../types";
+  import type { SortableEvent } from "sortablejs";
   import { onMount } from "svelte";
-  import Sortable from "sortablejs";
+  import Sortable from "./Sortable.svelte";
   import Checkbox from "./Checkbox.svelte";
   import EditableText from "./EditableText.svelte";
   import TrashbinIcon from "./icons/TrashbinIcon.svelte";
   import { getItemFromChromeStorage, saveItemOnChromeStorage } from "../lib/chrome";
   import { NAMESPACE_USER_TODOS } from "../constants";
 
-  let { todos }: { todos: Todo[] } = $props();
+  let { todos = $bindable() }: { todos: Todo[] } = $props();
   let addInputKey = $state(0);
-
-  let sortable: Sortable | null = null;
-  let todoULElement: HTMLUListElement;
 
   onMount(() => {
     getItemFromChromeStorage(NAMESPACE_USER_TODOS, (value) => {
@@ -21,45 +19,6 @@
         todos.push(...storedTodos);
       }
     });
-  });
-
-  onMount(() => {
-    if (!todoULElement) return;
-    if (!sortable) {
-      sortable = Sortable.create(todoULElement, {
-        // animation: 150,
-        onEnd: (evt) => {
-          if (
-            evt.oldIndex !== undefined &&
-            evt.newIndex !== undefined &&
-            evt.oldIndex !== evt.newIndex
-          ) {
-            const sortOrder = sortable!.toArray();
-
-            const reorderedTodos: Todo[] = [];
-            sortOrder.forEach((todoId) => {
-              const todo = todos.find((t) => t.id === todoId);
-              if (todo) reorderedTodos.push(todo);
-            });
-
-            if (reorderedTodos.length === todos.length) {
-              todos.length = 0;
-              todos.push(...reorderedTodos);
-              persistTodos();
-
-              console.log(todos);
-            }
-          }
-        },
-      });
-    }
-
-    return () => {
-      if (sortable) {
-        sortable.destroy();
-        sortable = null;
-      }
-    };
   });
 
   function persistTodos() {
@@ -115,12 +74,33 @@
       persistTodos();
     }
   }
+
+  function reOrderItems<T>(items: T[], oldIndex?: number, newIndex?: number) {
+    if (oldIndex === undefined || newIndex === undefined) return items;
+    if (oldIndex === newIndex) return items;
+
+    const copy = $state.snapshot(items);
+
+    const [movedItem] = copy.splice(oldIndex, 1);
+    copy.splice(newIndex, 0, movedItem);
+
+    return copy;
+  }
 </script>
 
 <div class="mt-30 flex flex-col gap-y-3 w-full items-center">
-  <ul bind:this={todoULElement} class="flex flex-col w-full max-w-xl items-start gap-y-3">
-    {#each todos as todo (todo.id)}
-      <li class="flex items-center w-full group" data-id={todo.id}>
+  <Sortable
+    options={{
+      onEnd: (evt: SortableEvent) => {
+        const { oldIndex, newIndex } = evt;
+        todos = reOrderItems(todos, oldIndex, newIndex);
+        persistTodos();
+      },
+    }}
+    class="flex flex-col w-full max-w-xl items-start gap-y-3"
+  >
+    {#each todos as todo (todo)}
+      <li class="flex items-center w-full group">
         <Checkbox
           id={todo.item}
           isChecked={todo.completed}
@@ -144,7 +124,7 @@
         </button>
       </li>
     {/each}
-  </ul>
+  </Sortable>
   <div class="add-new-todo-input-block flex w-full max-w-xl">
     <label class="flex items-center gap-x-2.5 cursor-pointer text-4xl">
       <input
